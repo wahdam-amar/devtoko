34308a9df2de353348c54814abe7dfba54dff8d1
wahdam <wahdam.amarulloh@gmail.com>
2021-03-07 10:28:30 +0700

    migration more4

---
+++ database/migrations/2021_03_07_031624_create_customer_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone')->unique()->nullable();
+            $table->string('city');
+            $table->string('status');
+            $table->index('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
---
+++ database/migrations/2021_03_07_031630_create_stock_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('desc')->nullable();
+            $table->bigInteger('amount');
+            $table->bigInteger('price_buy');
+            $table->bigInteger('prie_sell');
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('stock_category');
+            $table->string('status');
+            $table->index(['category_id', 'status']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock');
+    }
+}
