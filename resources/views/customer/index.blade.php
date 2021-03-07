f245601648e07a2ff0cf3936d79bd93d854a5fc1
wahdam <wahdam.amarulloh@gmail.com>
2021-03-07 20:33:01 +0700

    add modal for delete

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -127,14 +127,11 @@
                                             {{-- <button
                                                 class="px-2 py-1 bg-red-500 hover:bg-red-600 text-white text-sm font-medium rounded">Delete
                                             </button> --}}
-                                            <form class="inline-block" action="{{ route('customer.destroy',$item->id)}}"
-                                                method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" onclick="confirm('Hapus {{ $item->name }} ?')"
-                                                    class="px-2 py-1 bg-red-500 hover:bg-red-600 text-white text-sm font-medium rounded">Delete
-                                                </button>
-                                            </form>
+                                            <button onclick="openModal('{{ $item->name }}',{{ $item->id }})"
+                                                class="px-2 py-1 bg-red-500 hover:bg-red-600 text-white text-sm font-medium rounded">Delete
+                                            </button>
+
+
                                         </td>
                                     </tr>
                                     @endforeach
@@ -143,18 +140,50 @@
 
                         </div>
                         {{ $customer->links() }}
-
-                        {{-- form delete --}}
-                        <form action="{{ route('customer.destroy',1)}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger" type="submit">Delete</button>
-                        </form>
                     </div>
                 </div>
             </div>
         </div>
 
+        {{-- modal --}}
+        <div class="main-modal fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster"
+            style="background: rgba(0,0,0,.7);">
+            <div
+                class="border border-teal-500 shadow-lg modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+                <div class="modal-content py-4 text-left px-6">
+                    <!--Title-->
+                    <div class="flex justify-between items-center pb-3">
+                        <p class="text-2xl font-bold">Delete</p>
+                        <div class="modal-close cursor-pointer z-50">
+                            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18"
+                                height="18" viewBox="0 0 18 18">
+                                <path
+                                    d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+                                </path>
+                            </svg>
+                        </div>
+                    </div>
+                    <!--Body-->
+                    <div class="my-5 modal-body">
+                        <p></p>
+                    </div>
+                    <!--Footer-->
+                    <div class="flex justify-end pt-2">
+                        <button
+                            class="focus:outline-none modal-close px-4 bg-gray-400 p-3 rounded-lg text-black hover:bg-gray-300">Cancel</button>
+                        {{-- form delete --}}
+                        <form class="form-delete" action="{{ route('customer.destroy',1)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button
+                                class="focus:outline-none px-4 bg-teal-500 p-3 ml-3 rounded-lg text-white hover:bg-teal-400"
+                                type="submit">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- end modal --}}
 
     </div>
 </div>
@@ -233,4 +262,41 @@
           }
 </script>
 
+{{-- script for modal --}}
+<script>
+    const modal = document.querySelector('.main-modal');
+    const closeButton = document.querySelectorAll('.modal-close');
+    const modalBody = document.querySelectorAll('.modal-body p');
+    const formDelete = document.querySelectorAll('.form-delete');
+
+    const modalClose = () => {
+        modal.classList.remove('fadeIn');
+        modal.classList.add('fadeOut');
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 500);
+    }
+
+    const openModal = (name,id) => {
+        console.log(`${name} ${id}`);
+        modalBody[0].textContent= `Apa anda ingin menghapus ${name} ?`;
+        formDelete[0].action=`http://localhost:8000/customer/${id}`;
+        modal.classList.remove('fadeOut');
+        modal.classList.add('fadeIn');
+        modal.style.display = 'flex';
+    }
+
+    for (let i = 0; i < closeButton.length; i++) {
+
+        const elements = closeButton[i];
+
+        elements.onclick = (e) => modalClose();
+
+        modal.style.display = 'none';
+
+        window.onclick = function (event) {
+            if (event.target == modal) modalClose();
+        }
+    }
+</script>
 @endpush
