4f545e509a50beedba3fdca6085611d7e6b252f5
wahdam <wahdam.amarulloh@gmail.com>
2021-04-06 02:13:28 +0700

    simple db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(1)->create();
-        \App\Models\Customer::factory(1000)->create();
+        \App\Models\Customer::factory(10)->create();
         \App\Models\Supplier::factory(10)->create();
         \App\Models\Ekspedisi::factory(3)->create();
         \App\Models\Jasa::factory(3)->create();
