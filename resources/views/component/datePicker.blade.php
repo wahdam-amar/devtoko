09b726507cbe9aff35246f784bd763a53ff03b03
wahdam <wahdam.amarulloh@gmail.com>
2021-04-24 10:15:17 +0700

    Better Desc for date

--- resources/views/component/datePicker.blade.php
+++ resources/views/component/datePicker.blade.php
@@ -4,12 +4,12 @@
         <div class="w-full overflow-hidden md:my-4 md:px-4 md:w-1/2 my-2">
             <input type="text" name="startdate"
                 class="flatpickr flatpickr-input pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                placeholder="Select Date..">
+                placeholder="Tanggal Awal">
         </div>
         <div class="w-full overflow-hidden md:my-4 md:px-4 md:w-1/3 my-2">
             <input type="text" name="enddate"
                 class="flatpickr flatpickr-input pr-4 pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                placeholder="Select Date..">
+                placeholder="Tanggal Akhir">
         </div>
         <div class="w-full overflow-hidden md:my-4 md:px-4 md:w-1/6 my-2">
             <button type="submit"
