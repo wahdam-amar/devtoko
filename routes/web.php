37ae44da9473fa66711bd6fe0e6ef12d1922936a
wahdam <wahdam.amarulloh@gmail.com>
2021-04-24 10:24:27 +0700

    make route more readable

--- routes/web.php
+++ routes/web.php
@@ -19,37 +19,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Auth::routes();
 
 Route::middleware(['auth'])->group(function () {
-
     Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-    /**
-     * Customer
-     */
     Route::resource('customer', \App\Http\Controllers\CustomerController::class);
+
     Route::get('/json/customer', [\App\Http\Controllers\CustomerController::class, 'indexJson'])->name('customer.json');
+
     Route::get('customer/{customer}/invoice', [\App\Http\Controllers\CustomerController::class, 'invoice'])->name('customer.invoice');
 
     Route::resource('supplier', \App\Http\Controllers\SupplierController::class);
+
     Route::resource('ekspedisi', \App\Http\Controllers\EkspedisiController::class);
+
     Route::resource('jasa', \App\Http\Controllers\JasaController::class);
 
-    /**
-     * Stock
-     */
     Route::resource('stock', \App\Http\Controllers\StockController::class);
+
     Route::get('/json/stock', [\App\Http\Controllers\StockController::class, 'indexJson'])->name('stock.json');
 
     Route::get('transaction', [\App\Http\Controllers\StockController::class, 'transaction'])->name('stock.transaction');
+
     Route::post('transaction', [\App\Http\Controllers\StockController::class, 'saveTransaction'])->name('stock.save');
 
     Route::resource('invoice', \App\Http\Controllers\InvoiceController::class);
+
     Route::resource('category', \App\Http\Controllers\CategoryController::class);
 });
-
 
 /**
  * Kebutuhan Dev
@@ -65,7 +63,6 @@ Route::get('/custom', function () {
     //  Artisan::call('migrate:fresh --seed');
     // return Artisan::output();
 })->name('custom');
-
 
 /**
  * Test 
