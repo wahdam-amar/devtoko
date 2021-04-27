6ad69aee2527c6d0631fd9b5db29364c53846fab
wahdam <wahdam.amarulloh@gmail.com>
2021-04-27 21:23:56 +0700

    clear telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('telescope:prune')->hourly();
+        $schedule->command('telescope:clear')->everyTwoHours();
     }
 
     /**
